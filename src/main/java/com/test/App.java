0676027092576cb6fd66f6b5dcb3d0816a822981
Amarjeet Kumar Singh <53248566+amarjeetcs@users.noreply.github.com>
2022-07-25 13:49:46 +0530

    How to create connecion with hibernate Demo
    In this, you will be able to create a connection with Hibernate.

---
+++ src/main/java/com/test/App.java
@@ -0,0 +1,20 @@
+package com.test;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Configuration cfg = new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        SessionFactory factory = cfg.buildSessionFactory();
+        System.out.println(factory);
+        
+    }
+}
